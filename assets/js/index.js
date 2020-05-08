72b0c1a0da2cf05f6beb0b3dc7ccbd7f2d1d2e67
alicia centeno <alicent.08.02@gmail.com>
2020-05-07 21:42:28 -0400

    creacion de archivos principales y estructura del portafolio

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+window.scroll({
+    top: 2500,
+    left: 0,
+    behavior: 'smooth'
+});
+
+window.scrollBy({
+    top: 100,
+    left: 0,
+    behavior: 'smooth'
+});
+
+document.querySelector('#description', '#my-experience', '#my-jobs').scrollIntoView({
+    behavior: 'smooth'
+});
+
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip()
+})
